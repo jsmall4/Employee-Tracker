72fae2b145655f080b182180122bdd7aae2e257f
Jack Smallwood <jacksmallwood@icloud.com>
2022-09-22 13:12:19 +0100

    new index.js file instead of file, filled out sql schema, and db file

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,37 @@
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+-- console.table(department)
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id),
+    ON DELETE SET NULL
+);
+
+-- console.table(role)
+
+CREATE TABLE employee (
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   first_name VARCHAR(30) NOT NULL,
+   second_name VARCHAR(30) NOT NULL,
+   role_id INT,
+   manager_id INT,
+   FOREIGN KEY (role_id) REFERENCES role_id ON DELETE SET NULL,
+   FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+)
+
+-- console.table(employee),
+-- console.table(employee_db)
+
+
+
+
+
 source db/seeds.sql;
---
+++ index.js
@@ -0,0 +1,3 @@
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const inquirer = require("inquirer");
+const fs = require("fs");
